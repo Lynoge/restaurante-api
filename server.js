a1e3fcdf6887d00c2914c1940e7adf79213d75b2
Rodrigo Boniatti <boniatti.rodrigo@gmail.com>
2017-11-25 21:38:45 -0200

    Fist commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+
+app.set("port", process.env.PORT || 3001);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+
+app.get("/api", (req, res) => {
+    res.json({
+      data: "Hello guys"
+    });
+    return;
+});
+
+app.listen(app.get("port"), () => {
+  console.log(`Find the server at: http://localhost:${app.get("port")}/`); // eslint-disable-line no-console
+});
